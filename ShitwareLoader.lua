dae91349ce5a7c19b88dd9b3c680eedfc595a54b
SimpSupreme <108352188+SimpSupreme@users.noreply.github.com>
2025-07-28 14:47:50 -0500

    added an out-of-game message

--- ShitwareLoader.lua
+++ ShitwareLoader.lua
@@ -1,6 +1,13 @@
 local placeID = globals.place_id()
 if not placeID then return end
-if placeID ~= 12411473842 then
+local supportedPlaceIDs = {12552538292, 18259975825}
+
+local gameSet = {}
+for _, name in pairs(supportedPlaceIDs) do
+    gameSet[name] = true
+end
+
+if gameSet[placeID] then
     http.get("https://raw.githubusercontent.com/SimpSupreme/custom-assembly-scripts/refs/heads/main/loaderScripts/"..placeID..".lua", function(v)
         local success,results = loadstring(v)()
         if success then
@@ -9,6 +16,8 @@ if placeID ~= 12411473842 then
     end)
 elseif placeID == 12411473842 then
     print("Please load the pressure script when in-game <3")
+elseif placeID == 0 then
+    print("Please unload the script and load it again when in a game <3")
 end
 print("Made by @SINpathy in the discord :3")
 print("all scripts are open source, find them at https://github.com/SimpSupreme/custom-assembly-scripts")
