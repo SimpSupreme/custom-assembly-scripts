37d278be6feda40157d0294ed22175d02a95d4c9
SimpSupreme <108352188+SimpSupreme@users.noreply.github.com>
2025-07-27 19:18:43 -0500

    Pressure

---
+++ loaderScripts/12552538292.lua
@@ -0,0 +1,410 @@
+http.get("https://raw.githubusercontent.com/nfpw/Personal-Scripts/refs/heads/main/AssemblyOffsets.lua", function(v)
+    loadstring(v)()
+end)
+
+local last_click_time = 0
+local workspace = globals.workspace()
+local screenSize = render.screen_size()
+local gameplayFolder = workspace:FindChild("GameplayFolder")
+local roomsFolder = gameplayFolder:FindChild("Rooms")
+local anglerFont = render.create_font("C:\\Windows\\Fonts\\Verdana.ttf", 33, "ab")
+local majorFileNames = {"AbstractFile", "LunarDockDocument", "ThePainterDocument"}
+local fakeDoorNames = {"ServerTrickster", "TricksterRoom", "OutskirtsTrickster"}
+local itemNames = {"Flashlight", "RoomsBattery", "DefaultBattery3", "AltBattery3", "AltBattery2", "AltBattery1", "DefaultBattery1", "DefaultBattery2", "BigFlashBeacon", "Lantern", "SPRINT", "CodeBreacher", "ToyRemote", "BlueToyRemote", "Medkit", "HealthBoost", "WindupLight", "Gummylight"}
+local keycardNames = {"NormalKeyCard", "PasswordPaper", "InnerKeyCard", "RidgeKeyCard"}
+local anglerVariants = {"Angler", "Blitz", "Froger", "Pinkie", "Chainsmoker", "Pandemonium", "RidgeAngler", "RidgeFroger", "RidgeBlitz", "RidgePinkie", "RidgeChainsmoker", "A60"}
+local currencyNames = {"Blueprint", "Relic"}
+
+local warningsLabel = ui.label("Warnings")
+local anglerWarnToggle = ui.new_checkbox("Angler Warning")
+local fakeDoorWarnToggle = ui.new_checkbox("Fake Door Warning")
+local voidLockerWarnToggle = ui.new_checkbox("Void Locker Warning")
+
+local spacer = ui.label("")
+
+local highlightsLabel = ui.label("ESP/Highlights")
+local moneyESPToggle = ui.new_checkbox("Money ESP")
+local itemESPToggle = ui.new_checkbox("Item ESP")
+local keycardESPToggle = ui.new_checkbox("Keycard/Password ESP")
+local generatorESPToggle = ui.new_checkbox("Searchlight Generator ESP")
+
+local spacer2 = ui.label("")
+
+local autoLabel = ui.label("Automation")
+local autoShakeLabel = ui.label("Auto Shake Gummylight")
+local autoShakeKeybind = ui.keybind("Auto Shake")
+
+-- has to have multiple update timers, only money updates if it's all one. womp womp.
+local lastCurrencyUpdate = 0
+local lastItemUpdate = 0
+local lastKeycardUpdate = 0
+local monsterLockerUpdate = 0
+local fakeDoorUpdate = 0
+local generatorUpdate = 0
+local currencyCache = {}
+local itemPosCache = {}
+local itemNameCache = {}
+local keycardCache = {}
+local monsterLockerCache = {}
+local fakeDoorCache = {}
+local generatorCache = {}
+local updateInterval = 1
+
+local currencyNameSet = {}
+for _, name in pairs(currencyNames) do
+    currencyNameSet[name] = true
+end
+
+local function updateCurrencyCache()
+    currencyCache = {}
+
+    for _, room in pairs(roomsFolder:Children()) do
+        for _, model in pairs(room:Children()) do
+            if model:ClassName() == "Model" then
+                local spawnLocationsFolder = model:FindChild("SpawnLocations")
+                if spawnLocationsFolder then
+                    for _, spawn in pairs(spawnLocationsFolder:Children()) do
+                        for _, spawned in pairs(spawn:Children()) do
+                            if string.find(spawned:Name(), "^Currency") or currencyNameSet[spawned:Name()] then
+                                local prim = spawn:Primitive()
+                                if prim then
+                                    local worldPos = prim:GetPartPosition()
+                                    if worldPos then
+                                        table.insert(currencyCache, worldPos)
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+local keycardNameSet = {}
+for _, name in pairs(keycardNames) do
+    keycardNameSet[name] = true
+end
+
+local function updateKeycardCache()
+    keycardCache = {}
+
+    for _, room in pairs(roomsFolder:Children()) do
+        for _, model in pairs(room:Children()) do
+            if model:ClassName() == "Model" then
+                local spawnLocationsFolder = model:FindChild("SpawnLocations")
+                if spawnLocationsFolder then
+                    for _, spawn in pairs(spawnLocationsFolder:Children()) do
+                        local spawned = spawn:FindChildByClass("Model")
+                        if spawned then
+                            if keycardNameSet[spawned:Name()] then
+                                local prim = spawn:Primitive()
+                                if prim then
+                                    local worldPos = prim:GetPartPosition()
+                                    if worldPos then
+                                        table.insert(keycardCache, worldPos)
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+local itemNameSet = {}
+for _, name in pairs(itemNames) do
+    itemNameSet[name] = true
+end
+
+local function updateItemCache()
+    itemNameCache = {}
+    itemPosCache = {}
+
+    for _, room in pairs(roomsFolder:Children()) do
+        for _, model in pairs(room:Children()) do
+            if model:ClassName() == "Model" then
+                local spawnLocationsFolder = model:FindChild("SpawnLocations")
+                if spawnLocationsFolder then
+                    for _, spawn in pairs(spawnLocationsFolder:Children()) do
+                        local spawnModel = spawn:FindChildByClass("Model")
+                        if spawnModel then
+                            if itemNameSet[spawnModel:Name()] then
+                                local prim = spawn:Primitive()
+                                if prim then
+                                    local worldPos = prim:GetPartPosition()
+                                    if worldPos then
+                                        table.insert(itemPosCache, worldPos)
+                                        table.insert(itemNameCache, spawnModel:Name())
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+local function updateMonsterLockerCache()
+    monsterLockerCache = {} 
+
+    for _, room in pairs(roomsFolder:Children()) do
+        for _, models in pairs(room:Children()) do
+            if models:Name() == "MonsterLocker" then
+                local collisionBox = models:FindChild("LockerCollision")
+                if collisionBox then
+                    local prim = collisionBox:Primitive()
+                    if prim then
+                        local worldPos = prim:GetPartPosition()
+                        if worldPos then
+                            table.insert(monsterLockerCache, worldPos)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+local fakeDoorNameSet = {}
+for _, name in pairs(fakeDoorNames) do
+    fakeDoorNameSet[name] = true
+end
+
+local function updateFakeDoorCache()
+    fakeDoorCache = {} -- Clear previous data
+
+    for _, room in pairs(roomsFolder:Children()) do
+        for _, models in pairs(room:Children()) do
+            if fakeDoorNameSet[models:Name()] then
+                local doorInteractables = models:FindChild("Interactables")
+                if doorInteractables then
+                    local tricksterModel = doorInteractables:FindChild("Trickster")
+                    if tricksterModel then
+                        local doorModel = tricksterModel:FindChild("TricksterDoor")
+                        if doorModel then
+                            local doorRoot = doorModel:FindChild("Root")
+                            if doorRoot then
+                                local doorRootPrim = doorRoot:Primitive()
+                                if doorRootPrim then
+                                    local doorRootPos = doorRootPrim:GetPartPosition()
+                                    if doorRootPos then
+                                        table.insert(fakeDoorCache, doorRootPos)
+                                    end
+                                end
+                            end
+                        end    
+                    end
+                end    
+            end
+        end
+    end
+end
+
+local function generatorBrokenCheck(generatorModel)
+    local generatorFixedValue = generatorModel:FindChild("Fixed")
+    if not generatorFixedValue then return end
+    
+    local fixedValueAddress = generatorFixedValue:Address()
+    if not fixedValueAddress then return end
+    
+    local fixedIntValue = utils.read_memory("int", fixedValueAddress + Value)
+
+    if fixedIntValue < 100 then
+        return true
+    elseif fixedIntValue >= 100 then
+        return false
+    end
+end
+
+local function updateGeneratorCache()
+    generatorCache = {} -- Clear previous data
+
+    local encounterRoom = roomsFolder:FindChild("SearchlightsEncounter")
+    if encounterRoom then
+        local encounterInteractables = encounterRoom:FindChild("Interactables")
+        if encounterInteractables then
+            for _, models in pairs(encounterInteractables:Children()) do
+                local modelName = models:Name()
+                if modelName == "Generator" and generatorBrokenCheck(models) then
+                    local proxyPart = models:FindChild("ProxyPart")
+                    if proxyPart then
+                        local proxyPrim = proxyPart:Primitive()
+                        if proxyPrim then
+                            local proxyPos = proxyPrim:GetPartPosition()
+                            if proxyPos then
+                                table.insert(generatorCache, proxyPos)
+                            end
+                        end    
+                    end
+                end    
+            end
+        end
+    end
+end
+
+local function highlightCurrency()
+    local now = globals.curtime()
+
+    if now - lastCurrencyUpdate >= updateInterval then
+        updateCurrencyCache()
+        lastCurrencyUpdate = now
+    end
+
+    if not globals.is_focused() then return end
+
+    for _, worldPos in pairs(currencyCache) do
+        local screenPos = utils.world_to_screen(worldPos)
+        if screenPos.x > 0 then
+            render.text(screenPos.x, screenPos.y, "Currency", 255, 255, 255, 255, "", 0)
+        end
+    end
+end
+
+
+local function highlightItems()
+    local now = globals.curtime()
+
+    if now - lastItemUpdate >= updateInterval then
+        updateItemCache()
+        lastItemUpdate = now
+    end
+
+    if not globals.is_focused() then return end
+
+    for i = 1, #itemPosCache do
+        local worldPos = itemPosCache[i]
+        local screenPos = utils.world_to_screen(worldPos)
+        if screenPos.x > 0 then
+            local itemName = itemNameCache[i]
+            render.text(screenPos.x, screenPos.y, itemName, 0, 255, 0, 255, "", 0)
+        end
+    end
+
+end
+
+local function highlightKeycards()
+    local now = globals.curtime()
+
+    if now - lastKeycardUpdate >= updateInterval then
+        updateKeycardCache()
+        lastKeycardUpdate = now
+    end
+
+    if not globals.is_focused() then return end
+
+    for _, worldPos in pairs(keycardCache) do
+        local screenPos = utils.world_to_screen(worldPos)
+        if screenPos.x > 0 then
+            render.text(screenPos.x, screenPos.y, "Key", 0, 255, 255, 255, "", 0)
+        end
+    end
+end
+
+local function highlightMonsterLockers()
+        local now = globals.curtime()
+
+    if now - monsterLockerUpdate >= updateInterval then
+        updateMonsterLockerCache()
+        monsterLockerUpdate = now
+    end
+
+    if not globals.is_focused() then return end
+
+    for _, worldPos in pairs(monsterLockerCache) do
+        local screenPos = utils.world_to_screen(worldPos)
+        if screenPos.x > 0 then
+            render.text(screenPos.x, screenPos.y, "Void Locker", 255, 0, 255, 255, "", 0)
+        end
+    end
+end
+
+local function highlightFakeDoors()
+        local now = globals.curtime()
+
+    if now - fakeDoorUpdate >= updateInterval then
+        updateFakeDoorCache()
+        fakeDoorUpdate = now
+    end
+
+    if not globals.is_focused() then return end
+
+    for _, worldPos in pairs(fakeDoorCache) do
+        local screenPos = utils.world_to_screen(worldPos)
+        if screenPos.x > 0 then
+            render.text(screenPos.x, screenPos.y, "Fake Door", 255, 0, 0, 255, "", 0)
+        end
+    end
+end
+
+local function highlightGenerators()
+        local now = globals.curtime()
+
+    if now - generatorUpdate >= updateInterval then
+        updateGeneratorCache()
+        fakeDoorUpdate = now
+    end
+
+    if not globals.is_focused() then return end
+
+    for _, worldPos in pairs(generatorCache) do
+        local screenPos = utils.world_to_screen(worldPos)
+        if screenPos.x > 0 then
+            render.text(screenPos.x, screenPos.y, "Broken Generator", 225, 90, 90, 255, "", 0)
+        end
+    end
+end
+
+local function anglerWarn()
+    if not globals.is_focused() then return end
+    for _, i in pairs(anglerVariants) do
+        -- doesn't work with an inverted if, fuck if I know why
+        if workspace:FindChild(i) then
+            render.text((screenSize.x/2 - 50), (screenSize.y - 175), i, 255, 255, 0, 255, "", anglerFont)
+        end
+    end
+end
+
+cheat.set_callback("paint", function()
+    if anglerWarnToggle:get() then
+        anglerWarn()
+    end
+
+    if fakeDoorWarnToggle:get() then
+        highlightFakeDoors()
+    end
+
+    if voidLockerWarnToggle:get() then
+        highlightMonsterLockers()
+    end
+
+    if moneyESPToggle:get() then
+        highlightCurrency()
+    end
+
+    if itemESPToggle:get() then
+        highlightItems()
+    end
+
+    if keycardESPToggle:get() then
+        highlightKeycards()
+    end
+
+    if generatorESPToggle:get() then
+        highlightGenerators()
+    end
+
+    if autoShakeKeybind:get() then
+        local clicked_time = globals.curtime()
+        if clicked_time - last_click_time >= 0.4 then
+            input.click()
+            last_click_time = clicked_time
+            print("clicked")
+        end
+    end
+end)
