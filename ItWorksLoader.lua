81d7309546afa096d008849a093d14b53c7042d5
SimpSupreme <108352188+SimpSupreme@users.noreply.github.com>
2025-08-01 18:44:08 -0500

    add message for doors lobby

--- ItWorksLoader.lua
+++ ItWorksLoader.lua
@@ -9,17 +9,17 @@ end
 
 if gameSet[placeID] then
     http.get("https://raw.githubusercontent.com/SimpSupreme/custom-assembly-scripts/refs/heads/main/loaderScripts/"..placeID..".lua", function(v)
-        local success,results = loadstring(v)()
+        local success,result = loadstring(v)()
         if success then
-            results()
+            result()
         end
     end)
 elseif placeID == 12411473842 then
     print("Please load the pressure script when in-game <3")
+elseif placeID == 6516141723 then
+    print("Please load the Doors script when in-game <3")
 elseif placeID == 0 then
     print("Please unload the script and load it again when in a game <3")
-else
-    print("this game isn't supported :(")
 end
 print("Made by @SINpathy in the discord :3")
 print("all scripts are open source, find them at https://github.com/SimpSupreme/custom-assembly-scripts")
