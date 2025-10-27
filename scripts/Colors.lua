f9e9cce71e045741a9eb14f637216e4abd3d50d8
SimpSupreme <108352188+SimpSupreme@users.noreply.github.com>
2025-10-27 12:32:41 -0500

    Upload Color Library

---
+++ scripts/Colors.lua
@@ -0,0 +1,44 @@
+-- Neutrals
+local COLOR_WHITE          = {255, 255, 255}
+local COLOR_LIGHT_GREY     = {200, 200, 200}
+local COLOR_GREY           = {128, 128, 128}
+local COLOR_DARK_GREY      = {93, 93, 93}
+local COLOR_DARKER_GREY    = {64, 64, 64}
+local COLOR_BLACK          = {0, 0, 0}
+
+-- Primary & Secondary Colors
+local COLOR_RED            = {255, 0, 0}
+local COLOR_LIGHT_RED      = {255, 90, 90}
+local COLOR_DEEP_RED       = {93, 0, 0}
+local COLOR_GREEN          = {0, 255, 0}
+local COLOR_LIGHT_GREEN    = {90, 255, 90}
+local COLOR_BLUE           = {0, 0, 255}
+local COLOR_LIGHT_BLUE     = {90, 90, 255}
+local COLOR_YELLOW         = {255, 255, 0}
+local COLOR_LIGHT_YELLOW   = {255, 255, 90}
+local COLOR_CYAN           = {0, 255, 255}
+local COLOR_LIGHT_CYAN     = {90, 255, 255}
+local COLOR_MAGENTA        = {255, 0, 255}
+local COLOR_LIGHT_MAGENTA  = {255, 90, 255}
+
+-- Earthy / Natural Tones
+local COLOR_BROWN          = {139, 69, 19}
+local COLOR_TAN            = {210, 180, 140}
+local COLOR_BEIGE          = {245, 245, 220}
+local COLOR_SAND           = {194, 178, 128}
+local COLOR_OLIVE          = {128, 128, 0}
+local COLOR_FOREST_GREEN   = {34, 139, 34}
+local COLOR_SKY_BLUE       = {135, 206, 235}
+local COLOR_MAROON         = {128, 0, 0}
+local COLOR_PUMPKIN        = {215, 165, 32}
+
+-- Stylized / Special Colors
+local COLOR_ROYAL_NATION_PURPLE   = {185, 41, 240}
+local COLOR_GOLDEN_EMPIRE_YELLOW  = {241, 212, 41}
+local COLOR_ELECTRIC_BLUE         = {125, 249, 255}
+local COLOR_NEON_GREEN            = {57, 255, 20}
+local COLOR_LAVENDER              = {230, 230, 250}
+local COLOR_CRIMSON               = {220, 20, 60}
+local COLOR_CORAL                 = {255, 127, 80}
+local COLOR_SALMON                = {250, 128, 114}
+local COLOR_MINT                  = {152, 255, 152}
